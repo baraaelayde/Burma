f8e16b1397736e4835ef42a2d70b19cc0b5979ee
baraaelayde <baraaelaydy@Gmail.com>
2024-11-06 00:33:44 +0200

    Another important update to my code !!!

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -6,24 +6,24 @@ public class player
 {
     public string playerName;
     public int health;
-     private static int playerCount = 0;
+    private static int playerCount = 0;
     public void InitializePlayer(string name, int  initialHealth)
     {
         playerName = name;
-        initialHealth = health;
+        health = initialHealth;
         playerCount++;
     }
     public void Heal(int amount)
     {
         health += amount;
-        Debug.Log(playerName + ":" + amount);
+        Debug.Log(playerName+" : "+" Increasa health by : "+amount+" Health : "+health);
     }
     public void Heal(bool fullRestore)
     {
         if(fullRestore)
         {
             health = 100;
-            Debug.Log(playerName + ":" + health);
+            Debug.Log(playerName + " : " + health);
         }
     }
     public static void ShowPlayerCount()
