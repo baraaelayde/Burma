81ee892819025a2c91ef94c1f09b6922dfb5f03d
baraaelayde <baraaelaydy@Gmail.com>
2025-01-14 21:19:38 +0200

    Very important update to my code !!!

---
+++ Assets/script/ِِِAssignment24/RaceState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assignment24 {
+public enum RaceState 
+{
+    Start,
+    Accelerate,
+    Turn,
+    Crash,
+    Finish
+}
+}
